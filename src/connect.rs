23787403cd48dff1d6dfd15471c79d525c6f88e1
fattouche <alex.fattouche@gmail.com>
2019-07-15 19:03:33 -0600

    Use channels to communicate via threads

--- src/connect.rs
+++ src/connect.rs
@@ -1,53 +1,94 @@
-use laminar::{Socket, Packet, SocketEvent};
 use crossbeam_channel::{unbounded, Receiver, SendError, Sender, TryRecvError};
+use laminar::{Packet, Socket, SocketEvent};
 use std::net::SocketAddr;
-use std::thread;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::{thread, time};
 
-pub fn connect_as_sender(socket: &Socket, private_ip: String, public_ip: String) -> (Sender<Packet>, SocketAddr){
+pub fn connect_as_sender(
+    socket: &Socket,
+    private_ip: String,
+    public_ip: String,
+) -> (Sender<Packet>, SocketAddr) {
     let (packet_sender, event_reciever, addr) = connect(socket, private_ip, public_ip);
     (packet_sender, addr)
 }
 
-pub fn connect_as_reciever(socket: &Socket, private_ip: String, public_ip: String) -> (Receiver<SocketEvent>, SocketAddr){
+pub fn connect_as_reciever(
+    socket: &Socket,
+    private_ip: String,
+    public_ip: String,
+) -> (Receiver<SocketEvent>, SocketAddr) {
     let (packet_sender, event_reciever, addr) = connect(socket, private_ip, public_ip);
     (event_reciever, addr)
 }
 
-fn connect(socket: &Socket, private_ip: String, public_ip: String) -> (Sender<Packet>, Receiver<SocketEvent>, SocketAddr){
+fn connect(
+    socket: &Socket,
+    private_ip: String,
+    public_ip: String,
+) -> (Sender<Packet>, Receiver<SocketEvent>, SocketAddr) {
     let packet_sender = socket.get_packet_sender();
     let event_reciever = socket.get_event_receiver();
     let _thread = thread::spawn(move || socket.start_polling());
     let public_addr: SocketAddr = public_ip.parse().expect("Unable to parse socket address");
     let private_addr: SocketAddr = private_ip.parse().expect("Unable to parse socket address");
-    if !hole_punch(public_addr, packet_sender, event_reciever){
+    if !hole_punch(public_addr, packet_sender, event_reciever) {
         return (packet_sender, event_reciever, private_addr);
     }
-    (packet_sender, event_reciever,public_addr)
+    (packet_sender, event_reciever, public_addr)
 }
 
-fn hole_punch(addr: SocketAddr, packet_sender: Sender<Packet>, event_receiver: Receiver<SocketEvent>) -> bool{
-    let data = vec![1];
-    let unreliable = Packet::unreliable(addr, data);
-    let mut children = vec![];
-    //one thread to send packets, one to recieve
-    children.push(thread::spawn(move || {
-        let result = event_receiver.recv();
+fn hole_punch(
+    addr: SocketAddr,
+    packet_sender: Sender<Packet>,
+    event_receiver: Receiver<SocketEvent>,
+) -> bool {
+    let (channel_sender, channel_reciever) = mpsc::channel();
+    //one thread to recieve packets, main thread to send
+    let thread = thread::spawn(move || {
+        let result = event_receiver.recv_timeout(time::Duration::from_millis(5000));
         match result {
-    Ok(socket_event) => {
-        match  socket_event {
-            SocketEvent::Packet(packet) => {
-                let endpoint: SocketAddr = packet.addr();
-                let received_data: &[u8] = packet.payload();
-            },
-            SocketEvent::Connect(connect_event) => { /* a client connected */ },
-            SocketEvent::Timeout(timeout_event) => { /* a client timed out */},
+            Ok(socket_event) => {
+                match socket_event {
+                    SocketEvent::Packet(packet) => {
+                        let endpoint: SocketAddr = packet.addr();
+                        let received_data: &[u8] = packet.payload();
+                        if endpoint == addr {
+                            //We actually got the udp packet from the other side
+                            channel_sender.send(1);
+                        }
+                    }
+                    SocketEvent::Connect(connect_event) => { /* a client connected */ }
+                    SocketEvent::Timeout(timeout_event) => { /* a client timed out */ }
+                }
+            }
+            Err(e) => {
+                println!("Something went wrong when receiving, error: {:?}", e);
+            }
+        }
+        if result.is_err() {
+            channel_sender.send(0);
+        }
+    });
+
+    //Since this waits for the reciever to timeout, we don't need to join the threads
+    loop {
+        let data = vec![1];
+        let unreliable = Packet::unreliable(addr, data);
+        let millis = time::Duration::from_millis(500);
+        thread::sleep(millis);
+        packet_sender.send(unreliable).unwrap();
+        let result = channel_reciever.try_recv();
+        match result {
+            Ok(code) => {
+                if code == 0 {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            Err(e) => println!("error reading from channel: {:?}", e),
         }
     }
-    Err(e) => {
-        println!("Something went wrong when receiving, error: {:?}", e);
-    }
-}
-    }));
-    
-    true
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
-use laminar::{Socket, Packet, SocketEvent, Result};
 use crossbeam_channel::{unbounded, Receiver, SendError, Sender, TryRecvError};
+use laminar::{Packet, Result, Socket, SocketEvent};
 use std::thread;
 mod connect;
-
 
 fn main() -> Result<()> {
     let mut socket = Socket::bind("127.0.0.1:8000")?;
     let private_ip = String::from("127.0.0.1:9000");
     let public_ip = String::from("127.0.0.1:9001");
     let is_sender = true;
-    if is_sender{
-        let (sender,addr) = connect::connect_as_sender(&socket, private_ip, public_ip);
-        // send_file(sender, addr);
-    }else{
-        let (reciever,addr) = connect::connect_as_reciever(&socket, private_ip, public_ip);
+    if is_sender {
+        let (sender, addr) = connect::connect_as_sender(&socket, private_ip, public_ip);
+    // send_file(sender, addr);
+    } else {
+        let (reciever, addr) = connect::connect_as_reciever(&socket, private_ip, public_ip);
         // recieve_file(reciever,addr);
     }
-    
-    
+
     Ok(())
 }
 
-fn transfer_file(sender: Sender<Packet>, reciever:Receiver<SocketEvent>, is_sender:bool){
-    if is_sender{
+fn transfer_file(sender: Sender<Packet>, reciever: Receiver<SocketEvent>, is_sender: bool) {
+    if is_sender {
         let file_name = String::from("test.txt");
-        // send_file(filename, sender);
-    }else{
+    // send_file(filename, sender);
+    } else {
         recieve_file();
     }
 }
 
-fn send_file(filename: String, ){
+fn send_file(filename: String) {}
 
-}
-
-fn recieve_file(){
-
-}
+fn recieve_file() {}
