13a90392164e591091d1a410b66badcc95d4a403
fattouche <alex.fattouche@gmail.com>
2019-07-19 15:53:22 -0600

    Doing some testing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::SocketAddr;
+use std::str;
 use std::thread;
 mod connect;
 
@@ -27,40 +28,66 @@ fn main() -> Result<()> {
     let _thread = thread::spawn(move || socket.start_polling());
     let addr = connect::connect(&packet_sender, &event_reciever, private_ip, public_ip);
     if filename != "" {
-        send_file(filename, packet_sender, addr);
+        send_file(&filename, packet_sender, addr)?;
     } else {
-        recieve_file(filename, event_reciever, addr);
+        recieve_file(event_reciever, addr)?;
     }
     Ok(())
 }
 
-fn send_file(filename: String, sender: Sender<Packet>, addr: SocketAddr) {
+fn send_file(filename: &String, sender: Sender<Packet>, addr: SocketAddr) -> Result<()> {
+    let packet = Packet::reliable_sequenced(addr, filename.clone().into_bytes(), None);
+    sender.send(packet).unwrap();
     let f = File::open(filename).expect("Unable to open file");
     let f = BufReader::new(f);
-    let mut buffer = [0; 1024];
     for data in f.lines() {
+        println!("Sending data: {:?} to {:?}", data, addr);
         let data = data.expect("Unable to read data");
         let packet = Packet::reliable_sequenced(addr, data.into_bytes(), None);
         sender.send(packet).unwrap();
     }
+    Ok(())
 }
 
-fn recieve_file(filename: String, reciever: Receiver<SocketEvent>, addr: SocketAddr) {
-    let mut file = File::create(filename).expect("unable to create file");
+fn recieve_file(reciever: Receiver<SocketEvent>, addr: SocketAddr) -> Result<()> {
+    let result = reciever.recv();
+    let mut location = String::from("uninitialized");
+    println!("RESULT: {:?}", result);
+    match result {
+        Ok(socket_event) => match socket_event {
+            SocketEvent::Packet(packet) => {
+                let filename: &[u8] = packet.payload();
+                location = format!("{}-new", str::from_utf8(&filename).unwrap());
+                let file = File::create(&location).unwrap();
+            }
+            _ => return Err("Error getting filename from sender").unwrap(),
+        },
+        Err(err) => {
+            println!("Failed to get filename from sender, quitting: {:?}", err);
+            return Err(err).unwrap();
+        }
+    }
+    let mut writer = File::open(location).unwrap();
     loop {
+        println!("Waiting ");
         let result = reciever.recv();
         match result {
             Ok(socket_event) => match socket_event {
                 SocketEvent::Packet(packet) => {
                     let received_data: &[u8] = packet.payload();
-                    file.write_all(received_data)
-                        .expect("Failed to write to file");
+                    println!("RECIEVED SOME SHIT:{:?}", received_data);
+                    writer.write_all(received_data)?;
+                }
+                SocketEvent::Timeout(timeout_event) => {
+                    break;
                 }
                 _ => (),
             },
             Err(e) => {
                 println!("Something went wrong when receiving, error: {:?}", e);
+                return Err(e).unwrap();
             }
         }
     }
+    Ok(())
 }
